f6b1c334573620dc8893eee492a685315f73ee88
DVitsme <derrick.valentine@gmail.com>
2019-05-28 22:37:51 -0400

    Express Validator for users + Schema

---
+++ routes/api/users.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+// router.get('/', (req, res) => {
+//   res.send('Im alive');
+// });
+// @access Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please Enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // console.log(req.body);
+    res.send('User route');
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect To MongoDB Atlas
+connectDB();
 const PORT = process.env.PORT || 5000;
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
