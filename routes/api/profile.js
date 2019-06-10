d68eb4ace496c58e7acf156cabea2ddf602a86f0
DVitsme <derrick.valentine@gmail.com>
2019-06-10 02:56:48 -0400

    get logged in profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @access Public
-router.get('/', (req, res) => {
-  res.send('Profile route');
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
+
+// @access Private / get current user profile
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
